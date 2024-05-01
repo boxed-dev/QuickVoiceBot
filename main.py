5dba17fe6156ea5e7493392b99310f36f51de809
Rishabh <rishabh.kumar2022a@vitstudent.ac.in>
2024-05-01 11:28:09 +0530

    added groq for faster inference

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ load_dotenv()
 
 class LanguageModelProcessor:
     def __init__(self):
-        
+        self.llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
         # self.llm = ChatOpenAI(temperature=0, model_name="gpt-4-0125-preview", openai_api_key=os.getenv("OPENAI_API_KEY"))
-        self.llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125", openai_api_key=os.getenv("OPENAI_API_KEY"))
+        # self.llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
         self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
